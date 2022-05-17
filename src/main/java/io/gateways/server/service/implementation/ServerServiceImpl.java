9daeb09368a932f87335bbee514dfeb4dfd16937
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-05-17 15:42:02 -0300

    implement SeverService class

---
+++ src/main/java/io/gateways/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,77 @@
+package io.gateways.server.service.implementation;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.Random;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.gateways.server.enumeration.Status;
+import io.gateways.server.model.Server;
+import io.gateways.server.repo.ServerRepo;
+import io.gateways.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+
+
+    @Override
+    public Server create(Server server) {
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
+    }
+
+    @Override
+    public Server get(Long id) {
+        log.info("Fecthing server by id: {}", id);
+        return serverRepo.findById(id).get();
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);//If there is an existing id, it will update. 
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        log.info("Deleting server: {}", id);
+        serverRepo.deleteById(id);
+        return Boolean.TRUE;
+    }
+
+    private String setServerImageUrl() {
+        String[] imagesNames = {"server1.png", "server2.png", "server3.webp", "server4.jpg"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imagesNames[new Random().nextInt(4)]).toUriString();
+    }
+    
+    
+}
